98878fb345232774afc3445bcfdc8bad822bc5fd
Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>
2023-12-06 18:14:39 +0100

    refactor: fragment and vertex glsl code
    Co-authored-by: Martin Lettry <m.lettry1@gmail.com>
    
    Signed-off-by: Jeferson Morales Mariciano <jeferson.morales.mariciano@gmail.com>

--- assignment_9/template/shader/vertex.glsl
+++ assignment_9/template/shader/vertex.glsl
@@ -1,32 +1,25 @@
 #version 300 es
 
-in vec3 a_position;
-in vec3 a_color;
-in vec3 a_normal;
+in vec3 position;
+in vec3 color;
+in vec3 normal;
 
-out vec3 v_color;
-out vec3 v_normal;
-out vec3 v_viewDirection;
-out vec3 v_lightDirection;
+out vec3 frag_color;
+out vec3 frag_normal;
+out vec3 frag_view_direction;
+out vec3 frag_light_direction;
 
-
-uniform vec3 lightDirection;
+uniform vec3 light_direction;
 uniform mat4 modelMatrix;
 uniform mat4 projectionMatrix;
 uniform mat4 translationMatrix;
 uniform vec3 viewDirection;
 uniform mat4 viewMatrix;
 
-
-
 void main() {
-    v_color = a_color;
-
-
-    v_viewDirection = vec3(projectionMatrix * vec4(0.0, 0.0, -1.0, 0.0));
-    v_lightDirection = vec3(viewMatrix * vec4(lightDirection, 0.0));
-
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(a_position, 1.0);
-    v_normal = vec3(viewMatrix * modelMatrix * vec4(a_normal, 0.0));
-
+    frag_view_direction = vec3(projectionMatrix * vec4(0.0, 0.0, -1.0, 0.0));
+    frag_light_direction = vec3(viewMatrix * vec4(light_direction, 0.0));
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+    frag_normal = vec3(viewMatrix * modelMatrix * vec4(normal, 0.0));
+    frag_color = color;
 }
